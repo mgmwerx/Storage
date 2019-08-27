db0ad44cee3d088fe91022aa707c584e1b8af064
casenick <54515229+casenick@users.noreply.github.com>
2019-08-27 17:19:19 -0500

    Add files via upload
    Converted the two start and two end columns into one timestamp each. Updated the seed data to reflect the change

--- TableCreation.sql
+++ TableCreation.sql
@@ -7,8 +7,10 @@ create index event_locs on events (location_id);
 
 
 Use this(changing the number at the end) to create an index of each month)
-create index events_jan on events(start_date) where extract(month from start_date) = 1;--I'm not 100% sure this works so I conly created one.
+create index events_jan on events(starts_at) where extract(month from starts_at) = 1;--I'm not 100% sure this works so I conly created one.
 
+
+SELECT grantee, privilege_type FROM information_schema.role_table_grants WHERE table_name='events';
 
 */
 
@@ -16,7 +18,7 @@ create index events_jan on events(start_date) where extract(month from start_dat
 create table Locations(id serial, name varchar(50) default 'None', street_address varchar(60) not null, second_line varchar(20) default 'None', city varchar(15) not null, state varchar(2) not null, zip integer not null, primary key (id), unique (street_address, city, state, zip, name, second_line), CONSTRAINT zip_integrity check (zip > 9999)); 
 
 
-create table Events(title varchar(50) not null, location_id integer not null, start_date date not null, end_date date, start_time time not null, end_time time, description varchar(600), website_url varchar(500) not null, upload_time timestamp not null, primary key (location_id, start_date, start_time), foreign key (location_id) references Locations (id), unique(title, location_id),CONSTRAINT date_start_end_integrity CHECK (start_date <= end_date), CONSTRAINT time_start_end_integrity CHECK (start_time < end_time));
+create table Events(title varchar(50) not null, location_id integer not null, starts_at timestamp not null, ends_at timestamp, description varchar(600), website_url varchar(500) not null, upload_time timestamp not null, primary key (location_id, starts_at), foreign key (location_id) references Locations (id), unique(title, location_id),CONSTRAINT date_start_end_integrity CHECK (starts_at <= ends_at));
 
 
 /*This table was pulled out of the MVP. Future iterations could use a table of URLs to scrape from allowing more sites with minimum configration*/
